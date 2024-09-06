e23ef2e9e11883fec4da0553d434e3f99e0a46e0
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-09-06 11:21:08 -0300

    adding ✨ administradores table in database

--- src/database/create-tables.js
+++ src/database/create-tables.js
@@ -2,6 +2,16 @@ import { pool } from "./config.js"
 
 try {
    await pool.query(`
+      create table administradores (
+         login varchar(30) not null,
+         email varchar(50) not null,
+         senha varchar(30) not null,
+         nome varchar(40) not null,
+         ID varchar(20) not null
+
+         primary key (ID)
+      )
+
       create table categorias (
          ID varchar(20) not null,
          nome varchar(30) not null,

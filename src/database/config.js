21adfd270cc5cd21324ab8c31fa1f58d1b05bd2c
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-10-14 02:36:22 -0300

    editing ✨ postgres credentials

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,15 @@
 import pg from "pg"
+import 'dotenv/config'
 
 const { Pool } = pg
 
 const pool = new Pool({
-   host: "db",
-   port: 5432,
-   user: "amorzitos",
-   password: "amorzinhalindona",
-   database: "baucidb"
+   host: process.env.POSTGRES_HOST,
+   port: process.env.POSTGRES_PORT,
+   user: process.env.POSTGRES_USER,
+   password: process.env.POSTGRES_PASSWORD,
+   database: process.env.POSTGRES_DB,
+   ssl: { rejectUnauthorized: false }
 })
 
 pool.on("connect", () => console.log("Database connection successful"))

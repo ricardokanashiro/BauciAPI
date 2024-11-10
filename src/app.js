097ee2cc6c94db09b7418fe89e22f2191d334fcd
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-11-10 19:56:16 -0300

    adding ✨ preflight OPTIONS

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ import { router as routes } from "./routes/index.js"
 
 const app  = express()
 
-app.use(cors({
-   origin: 'http://localhost:5173'
-}))
+app.use(cors())
+app.options('*', cors())
+
 app.use(express.json())
 app.use("/", routes)
 app.use("/uploads", express.static("uploads"))

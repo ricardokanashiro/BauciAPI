a8be1bc67564648ba13e12d601a295b83ecfe8a7
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-11-10 20:00:46 -0300

    fixing ✨ cors configs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import { router as routes } from "./routes/index.js"
 
 const app  = express()
 
-app.use(cors())
+app.use(cors({
+   origin: 'http://localhost:5173',
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+   allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 app.options('*', cors())
 
 app.use(express.json())

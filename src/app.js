6a5c788d18cbfc67ea5369fe9ce6b53edd1ce627
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-11-10 19:44:08 -0300

    adding ✨ testing origin in cors config

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ import { router as routes } from "./routes/index.js"
 
 const app  = express()
 
-app.use(cors())
+app.use(cors({
+   origin: 'http://localhost:5173'
+}))
 app.use(express.json())
 app.use("/", routes)
 app.use("/uploads", express.static("uploads"))

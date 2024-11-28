3ca36ca0128c7046f3bc258be7f5bb4ee51bfe65
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-11-28 20:09:45 -0300

    adding ✨ vercel cors origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { router as routes } from "./routes/index.js"
 const app  = express()
 
 app.use(cors({
-   origin: 'http://localhost:5173',
+   origin: ['http://localhost:5173', 'https://bauci-web.vercel.app'],
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
